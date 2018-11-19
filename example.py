a3dbe53cb2bac3943ceefb0e2c3e9ca91aaf6ff4
Frederico B. Klein <frederico.klein@plymouth.ac.uk>
2018-11-19 14:29:09 +0000

    .

--- example.py
+++ example.py
@@ -28,8 +28,8 @@ class image_converter:
     if cols > 60 and rows > 60 :
       cv2.circle(cv_image, (50,50), 10, 255)
 
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(3)
+    #cv2.imshow("Image window", cv_image)
+    #cv2.waitKey(3)
 
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
