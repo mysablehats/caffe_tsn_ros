2fb15aa8dd176314625d23a104e00d0025e7b2a3
Frederico B. Klein <frederico.klein@plymouth.ac.uk>
2019-01-14 11:49:05 +0000

    master is passed as a parameter

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-
-export ROS_MASTER_URI=http://SATELLITE-S50-B:11311
+ROSMASTERNAME = $1
+shift
+export ROS_MASTER_URI=http://$ROSMASTERNAME:11311
 export ROS_IP=`hostname -I`
 export PYTHONPATH=/temporal-segment-networks/caffe-action/python/caffe:/temporal-segment-networks/:$PYTHONPATH
 
