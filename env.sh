523d89e812c12b8d46e49919370dc26993301cc5
fbk <frederico.klein@plymouth.ac.uk>
2019-01-14 12:08:16 +0000

    .

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-ROSMASTERNAME = $1
-shift
-export ROS_MASTER_URI=http://$ROSMASTERNAME:11311
+export ROS_MASTER_URI=http://$1:11311
 export ROS_IP=`hostname -I`
 export PYTHONPATH=/temporal-segment-networks/caffe-action/python/caffe:/temporal-segment-networks/:$PYTHONPATH
 
+#env | grep ROS
+shift
 exec /temporal-segment-networks/catkin_ws/devel/env.sh "$@"
