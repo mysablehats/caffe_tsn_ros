9b59867178da035c7a574d6ee6c0df30d2cec06a
Frederico B. Klein <frederico.klein@plymouth.ac.uk>
2019-02-11 10:05:31 +0000

    added service calls to setup package

--- class_tsn_rgb2.py
+++ class_tsn_rgb2.py
@@ -6,10 +6,10 @@ roslib.load_manifest('caffe_tsn_ros')
 import sys
 import rospy
 import cv2
+from caffe_tsn_ros.srv import *
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-
 import numpy as np
 np.set_printoptions(precision=2)
 #import matplotlib.pyplot as plt
@@ -20,33 +20,34 @@ mypath = "/temporal-segment-networks"
 sys.path.append(mypath)
 sys.path.append(os.path.abspath(mypath+"/tools"))
 sys.path.insert(0, mypath+'/lib/caffe-action/python') ## should use os.joinpath ...
+from pyActionRecog.action_caffe import CaffeNet
 import argparse
 import math
 import multiprocessing
 
-class image_converter:
-
+class tsn_classifier:
   def __init__(self):
     global mypath
-    self.image_pub = rospy.Publisher("class_overlay_image_raw",Image)
+    self.reconfig_srv_ = rospy.Service('reconf_split',split, self.reconfig_srv)
+    self.image_pub = rospy.Publisher("class_overlay_image_raw",Image, queue_size=1)
     self.label_pub = rospy.Publisher("action", String, queue_size=1)
-    self.dataset = rospy.get_param('~dataset')
-    self.device_id = rospy.get_param('~device_id')
-    self.split = rospy.get_param('~split')
-    self.videotopic = rospy.get_param('~video_topic')
-    self.classwindow = rospy.get_param('~classification_frame_window')
+    self.dataset = rospy.get_param('~dataset','hmdb51')
+    self.device_id = rospy.get_param('~device_id',0)
+    self.split = rospy.get_param('~split',1)
+    self.videotopic = rospy.get_param('~video_topic','videofiles/image_raw')
+    self.classwindow = rospy.get_param('~classification_frame_window',50)
     ###TODO: if it is another dataset, this list is different. will need to do an if ... thing here for the ufc101
-    self.actionlist = ['brush_hair','cartwheel','catch','chew','clap','climb','climb_stairs','dive','draw_sword','dribble','drink','eat','fall_floor','fencing','flic_flac','golf','handstand','hit','hug','jump','kick','kick_ball','kiss','laugh','pick','pour','pullup','punch','push','pushup','ride_bike','ride_horse','run','shake_hands','shoot_ball','shoot_bow','shoot_gun','sit','situp','smile','smoke','somersault','stand','swing_baseball','sword','sword_exercise','talk','throw','turn','walk','wave']
+    self.actionlist = rospy.get_param('action_list', ['brush_hair','cartwheel','catch','chew','clap','climb','climb_stairs','dive','draw_sword','dribble','drink','eat','fall_floor','fencing','flic_flac','golf','handstand','hit','hug','jump','kick','kick_ball','kiss','laugh','pick','pour','pullup','punch','push','pushup','ride_bike','ride_horse','run','shake_hands','shoot_ball','shoot_bow','shoot_gun','sit','situp','smile','smoke','somersault','stand','swing_baseball','sword','sword_exercise','talk','throw','turn','walk','wave'])
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber(self.videotopic, Image,self.callback,queue_size=1)
     from pyActionRecog.utils.video_funcs import default_aggregation_func
-    from pyActionRecog.action_caffe import CaffeNet
     self.defprox = default_aggregation_func
     self.frame_scores = []
     self.prototxt = mypath+'/models/'+ self.dataset +'/tsn_bn_inception_rgb_deploy.prototxt'
     self.caffemodel = mypath+'/models/'+ self.dataset +'_split_'+str(self.split)+'_tsn_rgb_reference_bn_inception.caffemodel'
     self.net = CaffeNet(self.prototxt, self.caffemodel, self.device_id)
     self.font = cv2.FONT_HERSHEY_SIMPLEX
+    print('hio')
     rospy.loginfo("waiting for callback from " + self.videotopic +" to do anything")
 
   def callback(self,data):
@@ -74,9 +75,20 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
 
+  def reconfig_srv(self, req):
+      self.image_sub.unregister()
+      self.split = req.Split
+      rospy.loginfo("reading split:"+str(req.Split))
+      #print(req.Split)
+      self.caffemodel = mypath+'/models/'+ self.dataset +'_split_'+str(self.split)+'_tsn_rgb_reference_bn_inception.caffemodel'
+      self.net = CaffeNet(self.prototxt, self.caffemodel, self.device_id)
+      self.image_sub = rospy.Subscriber(self.videotopic, Image,self.callback,queue_size=1)
+      #print('Dum')
+      return []
+
 def main(argss):
   rospy.init_node('action_classifier', anonymous=True)
-  ic = image_converter()
+  ic = tsn_classifier()
 
   try:
     rospy.spin()
