9f1e5fc990bb1df2936ff7b5a26b7a97a17268e4
Frederico B. Klein <frederico.klein@plymouth.ac.uk>
2019-02-13 14:47:15 +0000

    added publisher for instant classification values. untested

--- class_tsn_rgb2.py
+++ class_tsn_rgb2.py
@@ -30,14 +30,16 @@ class tsn_classifier:
     global mypath
     self.reconfig_srv_ = rospy.Service('reconf_split',split, self.reconfig_srv)
     self.image_pub = rospy.Publisher("class_overlay_image_raw",Image, queue_size=1)
+    self.label_fw_pub = rospy.Publisher("action_fw", String, queue_size=1)
     self.label_pub = rospy.Publisher("action", String, queue_size=1)
     self.dataset = rospy.get_param('~dataset','hmdb51')
     self.device_id = rospy.get_param('~device_id',0)
     self.split = rospy.get_param('~split',1)
     self.videotopic = rospy.get_param('~video_topic','videofiles/image_raw')
     self.classwindow = rospy.get_param('~classification_frame_window',50)
-    ###TODO: if it is another dataset, this list is different. will need to do an if ... thing here for the ufc101
     self.actionlist = rospy.get_param('action_list', ['brush_hair','cartwheel','catch','chew','clap','climb','climb_stairs','dive','draw_sword','dribble','drink','eat','fall_floor','fencing','flic_flac','golf','handstand','hit','hug','jump','kick','kick_ball','kiss','laugh','pick','pour','pullup','punch','push','pushup','ride_bike','ride_horse','run','shake_hands','shoot_ball','shoot_bow','shoot_gun','sit','situp','smile','smoke','somersault','stand','swing_baseball','sword','sword_exercise','talk','throw','turn','walk','wave'])
+    self.framesize_width = rospy.get_param('~framesize_width',340)
+    self.framesize_height = rospy.get_param('~framesize_height',256)
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber(self.videotopic, Image,self.callback,queue_size=1)
     from pyActionRecog.utils.video_funcs import default_aggregation_func
@@ -47,7 +49,7 @@ class tsn_classifier:
     self.caffemodel = mypath+'/models/'+ self.dataset +'_split_'+str(self.split)+'_tsn_rgb_reference_bn_inception.caffemodel'
     self.net = CaffeNet(self.prototxt, self.caffemodel, self.device_id)
     self.font = cv2.FONT_HERSHEY_SIMPLEX
-    print('hio')
+    #print('hio')
     rospy.loginfo("waiting for callback from " + self.videotopic +" to do anything")
 
   def callback(self,data):
@@ -60,15 +62,24 @@ class tsn_classifier:
     #(rows,cols,channels) = cv_image.shape
     #if cols > 60 and rows > 60 :
       #cv2.circle(cv_image, (50,50), 10, 255)
-    scores = self.net.predict_single_frame([cv_image,], 'fc-action', frame_size=(340, 256))
+    #TODO: frame_size should be a parameter
+    scores = self.net.predict_single_frame([cv_image,], 'fc-action', frame_size=(self.framesize_width, self.framesize_height))
     #print((scores))
+
+    #this publishes the instant time version, aka, per frame
+    self.label_fw_pub.publish(String(self.actionlist[np.argmax(self.defprox([scores]]))]))
+    rospy.logdebug("published the label for instant time version!")
+
+    #this part publishes the frame_window version
     self.frame_scores.append(scores)
     if len(self.frame_scores)>self.classwindow:
-        curract = self.actionlist[np.argmax(self.defprox(self.frame_scores))]
-        cv2.putText(cv_image,curract,(10,226), self.font, 1,(255,255,255),2)
+        curract_fw = self.actionlist[np.argmax(self.defprox(self.frame_scores))]
+        cv2.putText(cv_image,curract_fw,(10,226), self.font, 1,(255,255,255),2)
         self.frame_scores.pop(0)
-        self.label_pub.publish(String(curract))
-	rospy.logdebug("published the label!")
+        self.label_fw_pub.publish(String(curract_fw))
+	rospy.logdebug("published the label for the frame window version!")
+
+
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
       rospy.logdebug("published image")
